1c77e0c13a418c81b9a4cb823e7ee0348e7189a0
박성민 <moktak072@gmail.com>
2018-10-23 16:20:32 +0900

    2018.10.23 1st handout on Git

--- 20184256/main.c
+++ 20184256/main.c
@@ -1,15 +1,41 @@
-//
-//  main.c
-//  20184256
-//
-//  Created by 박성민 on 23/10/2018.
-//  Copyright © 2018 박성민. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int rfibo(int);
+int sfibo(int);
+
+int main()
+{
+    for(int i=3; i<=45; i++)
+    {
+        if(i%2 == 0)    //짝수
+        {
+            sfibo(i);       //실행만
+        }
+        else            //홀수
+        {
+            printf("rfibo(%d) = %d\n",i,rfibo(i));
+            printf("sfibo(%d) = %d\n",i,sfibo(i));
+        }
+    }
 }
+
+int rfibo(int n)    //recursive
+{
+    if(n <= 1)
+        return n;
+    else
+        return rfibo(n-1) + rfibo(n-2);
+}
+
+int sfibo(int n)    //static variable
+{
+    static int numb1 = 1;
+    static int numb2 = 1;
+    int result = numb1 + numb2;
+    
+    numb1 = numb2;
+    numb2 = result;
+    
+    return result;
+}
+
