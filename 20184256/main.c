6416e81d1fc6a9a66ce3d68471267691b6d989c7
박성민 <moktak072@gmail.com>
2018-10-23 16:18:41 +0900

    Initial Commit

---
+++ 20184256/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  20184256
+//
+//  Created by 박성민 on 23/10/2018.
+//  Copyright © 2018 박성민. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
